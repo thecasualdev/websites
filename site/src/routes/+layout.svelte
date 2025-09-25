ec11114fc0397502e4d4e01c1e911322413dc1c8
thecasualdev <git@thecasual.dev>
2025-09-25 13:28:12 +1000

    Testing

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+
 	import favicon from '$lib/assets/favicon.svg';
+	import './styles.css'
 
 	let { children } = $props();
+
 </script>
 
 <svelte:head>
+
 	<title>Hello, World!</title>
 	<link rel="icon" href={favicon} />
+	
 </svelte:head>
 
 {@render children?.()}
--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>websites, by thecasualdev</h1>
+<p>Hello, World!</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
