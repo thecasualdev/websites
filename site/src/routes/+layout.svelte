36e068d34b13b1c224c3713295f7a9b85a0e6f74
thecasualdev <git@thecasual.dev>
2025-09-25 13:13:35 +1000

    Project updated

---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>websites / thecasualdev</title>
+</svelte:head>
+
+{@render children?.()}
